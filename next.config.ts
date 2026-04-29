6489e6d41522e8e682915d147541af4948235b00
nissan <hexiliang04@gmail.com>
2026-04-29 16:18:48 +0900

    fix: confg

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/portfolio",
+  basePath: "/my-shopify-portfolio",
   images: { unoptimized: true },
 };
 
